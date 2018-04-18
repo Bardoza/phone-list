85959533e7ddf7a65a3be9eb71fc734e3efae330
Eduardo Obregon <eduardo.obz@gmail.com>
2018-04-18 18:04:37 -0500

    Add comments to Phone Item Component

--- src/components/PhoneItem.js
+++ src/components/PhoneItem.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+/**
+ *  Renders a simple phone number with a select field to change the phone type and a delete button
+ */
+
 class PhoneItem extends Component {
   static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    index: PropTypes.number.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    types: PropTypes.arrayOf(PropTypes.string).isRequired,
-    availableTypes: PropTypes.arrayOf(PropTypes.string).isRequired,
-    type: PropTypes.string.isRequired,
-    number: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired
+    onChange: PropTypes.func.isRequired, // Function to be called when there is a change in the phone type or phone number
+    index: PropTypes.number.isRequired, // Index of the current number on the parent state array
+    onDelete: PropTypes.func.isRequired, // Function to be called when the delete button is pressed
+    types: PropTypes.arrayOf(PropTypes.string).isRequired, // array of all phone types
+    availableTypes: PropTypes.arrayOf(PropTypes.string).isRequired, // array of all available phone types
+    type: PropTypes.string.isRequired, // the type of the current phone
+    number: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired // the number of the current form
   };
 
+  /**
+   * Calls the onChange props with the component index and current value from the select
+   */
   onTypeChange = e => {
     this.props.onChange(e.target.value, this.props.index, "type");
   };
 
+  /**
+   * Validate the phhone number and calls the onChange props with the current value and index
+   */
   onNumberChange = e => {
     const number = e.target.value.replace(/\D/gi, "");
     if (number.length > 15) {
